32a170c6406959fac038c14e096778881cb7e42f
FujishiroOtone <ariahatsune@outlook.com>
2018-10-23 15:15:33 +0800

    Aplayer

--- js/demo.js
+++ js/demo.js
@@ -1,110 +1,4 @@
-const ap1 = new APlayer({
-    element: document.getElementById('player1'),
-    mini: false,
-    autoplay: false,
-    lrcType: false,
-    mutex: true,
-    preload: 'metadata',
-    audio: [{
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        theme: '#ebd0c2'
-    }]
-});
-ap1.on('play', function () {
-    console.log('play');
-});
-ap1.on('play', function () {
-    console.log('play play');
-});
-ap1.on('pause', function () {
-    console.log('pause');
-});
-ap1.on('canplay', function () {
-    console.log('canplay');
-});
-ap1.on('playing', function () {
-    console.log('playing');
-});
-ap1.on('ended', function () {
-    console.log('ended');
-});
-ap1.on('error', function () {
-    console.log('error');
-});
 
-const ap2 = new APlayer({
-    element: document.getElementById('player2'),
-    mini: true,
-    autoplay: false,
-    lrcType: false,
-    mutex: true,
-    audio: [{
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
-        theme: '#ebd0c2'
-    }]
-});
-
-const ap3 = new APlayer({
-    element: document.getElementById('player3'),
-    mini: false,
-    autoplay: false,
-    lrcType: 3,
-    mutex: true,
-    audio: [{
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
-        theme: '#ebd0c2'
-    }]
-});
-
-const ap4 = new APlayer({
-    element: document.getElementById('player4'),
-    mini: false,
-    autoplay: false,
-    lrcType: false,
-    mutex: true,
-    theme: '#ad7a86',
-    order: 'random',
-    audio: [{
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
-        theme: '#ebd0c2'
-    }, {
-        name: 'トリカゴ',
-        artist: 'XX:me',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.lrc',
-        theme: '#46718b'
-    }, {
-        name: '前前前世',
-        artist: 'RADWIMPS',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.lrc',
-        theme: '#505d6b'
-    }, {
-        name: '光るなら(HLS)',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hls/hikarunara.m3u8',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        theme: '#ebd0c2',
-        type: 'hls'
-    }]
-});
 
 const ap5 = new APlayer({
     element: document.getElementById('player5'),
@@ -115,25 +9,7 @@ const ap5 = new APlayer({
     theme: '#e9e9e9',
     listFolded: false,
     listMaxHeight: 80,
-    audio: [{
-        name: '光るなら',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
-    }, {
-        name: 'トリカゴ',
-        artist: 'XX:me',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.lrc',
-    }, {
-        name: '前前前世',
-        artist: 'RADWIMPS',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.jpg',
-        lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.lrc',
-    }]
+    audio: []
 });
 const colorThief = new ColorThief();
 const setTheme = (index) => {
@@ -148,45 +24,6 @@ ap5.on('listswitch', (data) => {
     setTheme(data.index);
 });
 
-const ap6 = new APlayer({
-    element: document.getElementById('player6'),
-    mutex: true,
-    audio: [{
-        name: '光るなら(HLS)',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hls/hikarunara.m3u8',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        theme: '#ebd0c2',
-        type: 'hls'
-    }]
-});
-const ap7 = new APlayer({
-    element: document.getElementById('player7'),
-    mutex: true,
-    audio: [{
-        name: '光るなら(HLS)',
-        artist: 'Goose house',
-        url: 'https://moeplayer.b0.upaiyun.com/aplayer/hls/hikarunara.m3u8',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-        theme: '#ebd0c2',
-        type: 'customHls',
-    }],
-    customAudioType: {
-        'customHls': function (audioElement, audio, player) {
-            if (Hls.isSupported()) {
-                const hls = new Hls();
-                hls.loadSource(audio.url);
-                hls.attachMedia(audioElement);
-            }
-            else if (audioElement.canPlayType('application/x-mpegURL') || audioElement.canPlayType('application/vnd.apple.mpegURL')) {
-                audioElement.src = audio.url;
-            }
-            else {
-                player.notice('Error: HLS is not supported.');
-            }
-        }
-    }
-});
 
 const ap8 = new APlayer({
     element: document.getElementById('player8'),
